6a8bd32654cf9cbea7174f599f4cb28e586e5895
zivserphos <zivfromisrael@gmail.com>
2022-01-18 11:07:21 +0200

    small changes

--- my-app/src/components/main/Main.js
+++ my-app/src/components/main/Main.js
@@ -5,6 +5,7 @@ import CalculateResult from "../../modules/MathFunctions";
 
 export function Calculator() {
   const [exp, setExp] = useState("");
+  const [twoSigns, setTwoSigns] = useState(false);
 
   const addSignToExp = (sign) => {
     if (exp === "0") {
@@ -14,17 +15,21 @@ export function Calculator() {
     if (sign === "0") {
       const nums = exp.split(/[X/+-]/g);
       const lastNum = nums[nums.length - 1];
+      setTwoSigns(false);
       if (lastNum.startsWith("0") && lastNum[1] === undefined) return;
       return setExp(exp.concat(sign));
     }
 
     if (sign === "=") {
+      setTwoSigns(false);
       return setExp(CalculateResult(exp));
     }
     if (sign === "AC") {
+      setTwoSigns(false);
       return setExp("0");
     }
     if (sign === "DEL") {
+      setTwoSigns(false);
       return setExp(exp.slice(0, exp.length - 1));
     }
     if (sign === ".") {
@@ -33,13 +38,20 @@ export function Calculator() {
       const lastChar = exp[exp.length - 1];
       if (!lastNum.includes(".") && !isNaN(lastChar))
         return setExp(exp.concat(sign));
+      setTwoSigns(false);
       return;
     }
     if (isNaN(sign)) {
       const str = exp;
       const lastSign = str.charAt(str.length - 1);
-      if (isNaN(lastSign) && sign !== ".")
-        return setExp(exp.slice(0, exp.length - 1).concat(sign));
+      if (isNaN(lastSign) && sign !== ".") {
+        if ((lastSign === "X" || lastSign === "/") && sign === "-") {
+          setTwoSigns(true);
+          return setExp(exp.slice().concat(sign));
+        }
+        if (!twoSigns) return setExp(exp.slice(0, exp.length - 1).concat(sign));
+        return;
+      }
     }
     const newExp = exp.concat(sign);
     setExp(newExp);
--- my-app/src/modules/MathFunctions.js
+++ my-app/src/modules/MathFunctions.js
@@ -1,5 +1,5 @@
 function calculateResult(exp) {
-  let regex = /[0-9]*[.]?[0-9]+[X/][0-9]*[.]?[0-9]+/;
+  let regex = /[0-9]*[.]?[0-9]+[X/][-]?[0-9]*[.]?[0-9]+/;
   while (exp.match(regex)) {
     exp = exp.replace(regex, calculateExp(exp.match(regex)[0]));
   }
@@ -13,9 +13,13 @@ function calculateResult(exp) {
 function calculateExp(exp) {
   const isMinus = exp.charAt(0) === "-" ? true : false;
   if (isMinus) exp = exp.slice(1);
-  console.log(exp);
-  const numbers = exp.split(/[X/+-]/g);
-  const oper = exp.replace(/[0-9]*[.]?[0-9]/g, "");
+  const numbers = exp.split(/[X/+-][-]?/g);
+  console.log(numbers);
+  let oper = exp.replace(/[0-9]*[.]?[0-9]/g, "");
+  if (oper.length > 1) {
+    oper = oper.slice(0, 1);
+    numbers[1] *= -1;
+  }
   let [num1, num2] = [numbers[0], numbers[1]];
   if (isMinus) num1 = num1 * -1;
   if (oper === "X") {
